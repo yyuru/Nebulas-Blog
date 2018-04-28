6c1fc74da687f6f5e707ce8ddbdda384c7ed722f
nebulas <1198398580@qq.com>
2018-04-28 14:10:51 +0800

    'msg'

--- Nebulas/ClientApp/app/app.shared.module.ts
+++ Nebulas/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { LandingConponemt } from './components/landingpage/landingpage.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        LandingConponemt
     ],
     imports: [
         CommonModule,
@@ -27,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'landing', component: LandingConponemt },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ Nebulas/ClientApp/app/components/landingpage/landingpage.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'landingpage',
+    templateUrl: './landingpage.component.html'
+})
+
+export class LandingConponemt {
+
+}
